d7bc9ed0465e6b9996b02fe8ab096f4a6b20379a
ibrahim kurut <ibrahim.kurut06@gmail.com>
2024-10-15 21:08:06 +0300

    add total price to Reservation response

--- rental/serializers.py
+++ rental/serializers.py
@@ -14,9 +14,17 @@ class CarSerializer(serializers.ModelSerializer):
 class ReservationSerializer(serializers.ModelSerializer):
     customer = serializers.ReadOnlyField(source='customer.username')  # View the username instead of the ID
     car_name = serializers.ReadOnlyField(source='car.brand')  # Add a the name car to response
+    total_price = serializers.SerializerMethodField()  # Add total price field to the response
     class Meta:
         model = Reservation
-        fields = ['id', 'customer', 'car_name', 'car', 'start_date', 'end_date']
+        fields = ['id', 'customer', 'car_name', 'car', 'start_date', 'end_date','total_price']
+
+    def get_total_price(self, obj):
+        # Calculate the total days of the reservation
+        num_days = (obj.end_date - obj.start_date).days
+        # Multiply the number of days by the rent_per_day of the car
+        total_price = num_days * obj.car.rent_per_day
+        return total_price
 
 
 class UserSerializer(serializers.ModelSerializer):
